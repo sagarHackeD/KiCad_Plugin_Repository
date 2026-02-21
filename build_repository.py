986478f3e2d1808092c3ba829a9d41e611e8001d
Sagar Naik <sagarnaik430@googlemail.com>
2026-02-22 00:08:58 +0530

    script to generate repository.json

--- build_repository.py
+++ build_repository.py
@@ -1,12 +1,22 @@
-import io
+import hashlib
 import json
 import os
 from datetime import datetime
-from metadata.ci.validate.util.getsha import getsha256
+import zipfile
+
+READ_SIZE = 65536
+
+
+def getsha256(filename) -> str:
+    sha256 = hashlib.sha256()
+    with open(filename, "rb") as f:
+        while data := f.read(READ_SIZE):
+            sha256.update(data)
+    return sha256.hexdigest()
 
 
 def load_json_file(file_name: str) -> dict:
-    with io.open(file_name, encoding="utf-8") as f:
+    with open(file_name, encoding="utf-8") as f:
         return json.load(f)
 
 
@@ -17,14 +27,26 @@ def update(json_, file):
 
     print(f"Updating {file} with sha256 {sha} and timestamp {dt} and mtime {mtime}")
 
-    json_["url"] = f"https://raw.githubusercontent.com/sagarHackeD/KiCad_Plugin_Repository/refs/heads/main/{os.path.basename(file)}"
+    json_["url"] = (
+        f"https://raw.githubusercontent.com/sagarHackeD/KiCad_Plugin_Repository/refs/heads/main/{os.path.basename(file)}"
+    )
     json_["sha256"] = sha
     json_["update_timestamp"] = int(mtime)
     json_["update_time_utc"] = dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
-if __name__ == "__main__":
+def create_resources_zip(icon_files):
+    if not icon_files:
+        return
 
+    with zipfile.ZipFile(
+        "resources.zip", "w", zipfile.ZIP_DEFLATED, compresslevel=9
+    ) as z:
+        for path in icon_files:
+            z.write(path, os.path.relpath(path, "packages"))
+
+
+if __name__ == "__main__":
     packages = []
 
     for file in os.listdir("packages"):
@@ -33,22 +55,28 @@ if __name__ == "__main__":
 
     print(f"{len(packages)} packages processed")
 
-    with io.open("packages.json", "w", encoding="utf-8") as f:
+    with open("packages.json", "w", encoding="utf-8") as f:
         json.dump({"packages": packages}, f, indent=4)
 
     repo = load_json_file("repository.json")
     repo["name"] = "sagarHackeD KiCad Plugin Repository"
     update(repo["packages"], "packages.json")
 
+    create_resources_zip(
+        [
+            os.path.join("packages", file, "icon.png")
+            for file in os.listdir("packages")
+            if os.path.exists(os.path.join("packages", file, "icon.png"))
+        ]
+    )
+
     try:
-        if os.path.exists("resources.zip"):
-            update(repo["resources"], "resources.zip")
+        if os.path.exists("build/resources.zip"):
+            update(repo["resources"], "build/resources.zip")
         else:
             del repo["resources"]
     except KeyError:
         pass
 
-    with io.open("repository.json", "w", encoding="utf-8") as f:
+    with open("repository.json", "w", encoding="utf-8") as f:
         json.dump(repo, f, indent=4)
-
-    print("Repository should be available at repository.json")
